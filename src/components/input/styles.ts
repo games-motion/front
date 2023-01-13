984c0c4d089e415bf17e72ad2fabc5d4c359a5c1
Jeanluca-Moren0 <jeanlucam12@gmail.com>
2023-01-12 21:55:56 -0300

    fix: adjusting button font, padding and background

--- src/components/input/styles.ts
+++ src/components/input/styles.ts
@@ -5,22 +5,25 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: ${theme.space['2']};
+  z-index: 1;
 
   label {
-    font-size: 20px;
+    font-size: 16px;
     font-weight: ${theme.fontWeights.bold};
     text-transform: capitalize;
   }
+
   input {
     width: 100%;
     height: 40px;
     border-radius: ${theme.radius.sm};
     border: none;
     background: none;
-    border: 1px solid ${theme.colors.brand['blue-900']};
-    padding: 8px;
+    border: 1px solid ${theme.colors.brand['blue-800']};
+    background-color: ${theme.colors.gray[600]}66; // 66 in the end to put 40% of opacity
+    padding: ${theme.space[2]};
     color: ${theme.colors.gray['200']};
-    font-size: 16px;
+    font-size: 14px;
 
     :focus {
       border-color: ${theme.colors.brand['blue-600']};
