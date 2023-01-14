a5b5358b2a3c0bdfe511ad463be970b4c048e6cb
whyy <icarovsilva1@gmail.com>
2023-01-14 20:52:45 -0300

    feat: testing button

--- src/components/Button/index.spec.tsx
+++ src/components/Button/index.spec.tsx
@@ -40,4 +40,70 @@ describe('<Button/> ', () => {
 
     expect(mockFunction).toHaveBeenCalled()
   })
+
+  it('should background blue by solid variant', () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <Button variant="solid" onClick={mockFunction}>
+          Button
+        </Button>
+      </ThemeProvider>
+    )
+
+    const element = screen.getByText(/button/i)
+
+    expect(element).toHaveStyle({
+      background: theme.colors.brand['blue-500'],
+    })
+  })
+
+  it('should background transparent by ghost variant', () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <Button variant="ghost" onClick={mockFunction}>
+          Button
+        </Button>
+      </ThemeProvider>
+    )
+
+    const element = screen.getByText(/button/i)
+
+    expect(element).toHaveStyle({
+      background: 'transparent',
+    })
+  })
+
+  it('should background transparent by link variant', () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <Button variant="link" onClick={mockFunction}>
+          Button
+        </Button>
+      </ThemeProvider>
+    )
+
+    const element = screen.getByText(/button/i)
+
+    expect(element).toHaveStyle({
+      background: 'transparent',
+    })
+  })
+
+  it('should background transparent and have outline by outline variant', () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <Button variant="outline" onClick={mockFunction}>
+          Button
+        </Button>
+      </ThemeProvider>
+    )
+
+    const element = screen.getByText(/button/i)
+
+    expect(element).toHaveStyle({
+      background: 'transparent',
+      outline: `1px solid ${theme.colors.brand['blue-500']}`,
+      color: theme.colors.brand['blue-500'],
+    })
+  })
 })
